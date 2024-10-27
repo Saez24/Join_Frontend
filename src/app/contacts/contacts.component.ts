2956e00f14c531c41ab488016dc9d9d519a77a91
Alex <saez0724+github@googlemail.com>
2024-10-27 15:12:25 +0100

    working on dialogs

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../shared/services/backend/backend.service';
 import { Contacts } from '../shared/models/contacts.class';
-import { NgFor, NgStyle } from '@angular/common';
+import { NgFor, NgIf, NgStyle } from '@angular/common';
 
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [NgFor, NgStyle],
+  imports: [NgFor, NgStyle, NgIf],
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss',
+    '../shared/animations/animations.scss'
+  ]
 })
 export class ContactsComponent implements OnInit {
 
+  isOverlayVisible = false;
   users: Contacts[] = [];
 
 
@@ -25,8 +28,8 @@ export class ContactsComponent implements OnInit {
   getAllContacts() {
     this.api.getAll('names').subscribe({
       next: (results: any) => {
-        this.users = results;  // Versuche mögliche Pfade
-        console.log(this.users);  // Teste, ob die Zuweisung nun funktioniert
+        this.users = results.map((data: any) => new Contacts(data));
+        console.log(this.users);
       },
       error: (error) => {
         console.error('Fehler beim Abrufen der Daten:', error);
@@ -38,6 +41,36 @@ export class ContactsComponent implements OnInit {
     if (!name) return '';
     let initials = name.split(' ').map(part => part.charAt(0)).join('');
     return initials.toUpperCase();
+  }
+
+  slideInFromRight() {
+    const contactCont = document.getElementById('contact-cont');
+    contactCont?.classList.add('slide-in-from-right');
+
+    setTimeout(() => {
+      this.isOverlayVisible = true;
+      contactCont?.classList.remove('slide-in-from-right');
+    }, 300); // Timeout muss die gleiche Dauer wie die CSS-Animation haben
+  }
+
+  slideOutToRight() {
+    const contactCont = document.getElementById('contact-cont');
+    contactCont?.classList.add('slide-out-to-right');
+
+    setTimeout(() => {
+      this.isOverlayVisible = false;
+      contactCont?.classList.remove('slide-out-to-right');
+    }, 300); // Timeout muss die gleiche Dauer wie die CSS-Animation haben
+  }
+
+  closeOverlayWhenGreyAreaWasClicked(event: Event) {
+    if (event.target === document.getElementById('contact-overlay')) {
+      this.slideOutToRight();
+    }
+  }
+
+  createContact() {
+    // Logik zum Erstellen eines Kontakts
   }
 
 }
--- src/app/shared/models/contacts.class.ts
+++ src/app/shared/models/contacts.class.ts
@@ -1,12 +1,26 @@
-
 export class Contacts {
     id: string;
     email: string | null;
     name: string;
+    randomColor: string;
+
+    // Liste der verfügbaren Farben
+    private static readonly colors = [
+        '#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8',
+        '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701',
+        '#0038FF', '#00FFFF', '#FF0000', '#FF4646', '#FFBB2B'
+    ];
 
     constructor(obj?: any) {
         this.id = obj ? obj.id : null;
         this.email = obj ? obj.email : null;
         this.name = obj ? obj.name : null;
+        this.randomColor = Contacts.getRandomColor(); // Zufällige Farbe zuweisen
+    }
+
+    // Statische Methode zum Auswählen einer zufälligen Farbe
+    private static getRandomColor(): string {
+        const colors = Contacts.colors;
+        return colors[Math.floor(Math.random() * colors.length)];
     }
 }
